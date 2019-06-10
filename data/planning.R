884e5c50c3d13940b0474921a2a69071d31cf269
jsta <stachel2@msu.edu>
2019-06-10 15:01:45 -0400

    add gdoc planning script

---
+++ data/planning.R
@@ -0,0 +1,8 @@
+# collapse log file by state
+#
+# 1. take files with a lagoslakeid field
+# 2. check if fields contain depth from the provider
+# 2. bind rows
+# 3. select lat, lon, name, and llid columns
+
+
