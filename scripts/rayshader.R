5f402cd67774675e40c75f2132cf33230b1faee9
jsta <stachel2@msu.edu>
2019-09-10 15:06:48 -0400

    towards rayshader panel figure

--- scripts/rayshader.R
+++ scripts/rayshader.R
@@ -6,6 +6,8 @@ library(mapview)
 library(raster)
 library(rayshader)
 library(elevatr)
+library(ggpointdensity)
+library(ggplot2)
 
 # Lake Bathymetric Digital Elevation Model (DEM): A digital elevation
 # model (DEM) representing lake bathymetry. Cell size is most often 5m,
@@ -21,45 +23,74 @@ r <- raster("data/mn_bathy/lake_bathymetric_elevation_model.tif")
 
 lg_raw <- lagosne_load()
 lg     <- left_join(lg_raw$locus, lg_raw$state) %>%
+  left_join(dplyr::select(lg_raw$lakes_limno, lagoslakeid, maxdepth, maxdepthsource)) %>%
   dplyr::filter(state == "MN") %>%
-  arrange(desc(lake_area_ha)) %>%
-  filter(lake_area_ha < 1200) %>%
-  filter(lake_area_ha >= 4) %>%
+  arrange(desc(maxdepth)) %>%
+  filter(lake_area_ha < 5000) %>%
+  filter(lake_area_ha >= 100) %>%
   dplyr::select(lagoslakeid, gnis_name,
-                nhd_lat, nhd_long, lake_area_ha)
+                nhd_lat, nhd_long, lake_area_ha, maxdepth, maxdepthsource)
 
-res_sf   <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", 1183)
-res_sf   <- st_transform(res_sf, 26915)
-box      <- raster::extent(
-  st_buffer(st_sf(st_as_sfc(st_bbox(res_sf))), 50))
-rsub     <- crop(r, box)
-maxdepth <- abs(cellStats(rsub, "min"))
+quantile_sample <- function(x, y = NA, probs){
+  x_qs   <- quantile(x, probs, na.rm = TRUE)
+  x_diff <- sapply(x_qs, function(b) abs(b - x))
 
-# TODO: replace NA with data from elevatr
-elev <- get_elev_raster(rsub, 13)
-test <- crop(elev, rsub)
-test <- mask(test, test <= (cellStats(test, "min") + 1), maskvalue = TRUE)
-test <- test - cellStats(test, "min")
-test <- resample(test, rsub)
+  if(!is.na(y[1])){
+    y_qs <- quantile(y, probs, na.rm = TRUE)
+    y_diff <- sapply(y_qs, function(b) abs(b - x))
 
-test2 <- merge(test, rsub)
-test2[is.na(test2)] <- 0
+    total_diff_1 <- x_diff[,1] + y_diff[,1]
+    total_diff_2 <- x_diff[,2] + y_diff[,2]
 
-# plot(rsub)
-# mapview(res_sf)
+    data.frame(x = c(which.min(total_diff_1),
+                     which.min(total_diff_2)),
+               y = c(which.min(total_diff_1),
+                     which.min(total_diff_2)),
+               row.names = probs)
 
-# rayshader
-rmat <- matrix(test2 + maxdepth,
-               nrow = ncol(rsub), ncol = nrow(rsub))
-rmat[rmat > maxdepth] <- NA
-# slice half off to show profile
-# rmat[1:(nrow(rmat)/2),] <- NA
+  }else{
+    apply(x_diff, 2, which.min)
+  }
+}
+
+llids <- lg[
+  quantile_sample(lg$lake_area_ha,lg$maxdepth, probs = probs)[,1],]$
+  lagoslakeid
+
+get_hypso <- function(llid){
+  res_sf   <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", llid)
+  res_sf   <- st_transform(res_sf, 26915)
+  box      <- raster::extent(
+    st_buffer(st_sf(st_as_sfc(st_bbox(res_sf))), 50))
+  rsub     <- crop(r, box)
+  maxdepth <- abs(cellStats(rsub, "min"))
+
+  # replace NA with data from elevatr
+  elev <- get_elev_raster(rsub, 13)
+  test <- crop(elev, rsub)
+  test <- mask(test, test <= (cellStats(test, "min") + 1), maskvalue = TRUE)
+  test <- test - cellStats(test, "min")
+  test <- resample(test, rsub)
+
+  test2 <- merge(test, rsub)
+  test2[is.na(test2)] <- 0
+
+  rmat <- matrix(test2 + maxdepth,
+                 nrow = ncol(rsub), ncol = nrow(rsub))
+  rmat[rmat > maxdepth] <- NA
+  # slice half off to show profile
+  # rmat[1:(nrow(rmat)/2),] <- NA
+  rmat
+}
+
+hypso_1 <- get_hypso[llids[1]]
+
 
 tex <- create_texture("#AFEEEE", "#B0C4DE", "#B0E2FF", "#B2DFEE", "#BCD2EE")
 rmat %>%
   sphere_shade(texture = tex) %>%
   plot_3d(rmat, water = TRUE, waterdepth = maxdepth,
-          zscale = 0.2, solidcolor = "white", solidalpha = 0.1,
+          zscale = 0.4, solidcolor = "white", solidalpha = 0.4,
           shadow = FALSE, solidlinecolor = "white", phi = 20)
 render_snapshot(clear = TRUE)
 
@@ -76,4 +107,3 @@ library(magick)
 image_write(image_append(c(
   image_trim(image_read("solidalpha1.0.png")),
   image_trim(image_read("soildalpha0.1.png")))), "solidalpha.png")
-
