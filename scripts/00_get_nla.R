6ce9019a7e9e3108d3b7b1da473794cd858f75ff
jsta <stachel2@msu.edu>
2019-09-17 13:53:24 -0400

    parse nla

--- scripts/00_get_nla.R
+++ scripts/00_get_nla.R
@@ -1,6 +1,33 @@
-library(googledrive)
+source("scripts/99_utils.R")
 
 # get nla files from the cont-limno depth folder and merge with data from nlaR
+# drive_download(file = "US_EPA_NLA2012_siteinfo_v1_linked_R",
+#                path = "data/00_nla/2012_nla.csv")
+# drive_download(file = "US_EPA_NLA2007_siteinfo_v1_linked_R",
+#                path = "data/00_nla/2007_nla.csv")
 
-"US_EPA_NLA2012_siteinfo_v1_linked_R"
-"US_EPA_NLA2007_siteinfo_v1_linked_R"
+dt_raw_2012 <- read_csv("data/00_nla/2012_nla.csv")
+dt_raw_2007 <- read_csv("data/00_nla/2007_nla.csv") #nla 2007 file already has depth
+
+nl2012 <- nla_load(2012)$wide_siteinfo %>%
+  dplyr::select(SITE_ID, LAT_DD83, LON_DD83) %>%
+  distinct(SITE_ID, .keep_all = TRUE) %>%
+  left_join(dplyr::select(nla_load(2012)$wide_profile,
+                          SITE_ID, INDEX_SITE_DEPTH)) %>%
+  tidyr::drop_na(INDEX_SITE_DEPTH) %>%
+  group_by(SITE_ID) %>%
+  slice(which.max(INDEX_SITE_DEPTH)) %>%
+  LAGOSNE::coordinatize("LAT_DD83", "LON_DD83")
+
+dt_2012 <- dt_raw_2012 %>%
+  distinct(SITE_ID, .keep_all = TRUE) %>%
+  left_join(nl2012) %>%
+  dplyr::filter(!is.na(INDEX_SITE_DEPTH)) %>%
+  data.frame(stringsAsFactors = FALSE)
+
+dt_2007 <- dt_raw_2007 %>%
+  distinct(SITE_ID, .keep_all = TRUE) %>%
+  dplyr::filter(!is.na(DEPTHMAX)) %>%
+  data.frame(stringsAsFactors = FALSE)
+
+
