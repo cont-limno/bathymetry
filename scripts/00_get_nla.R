477406e275535a22f3c51267a710fe2b2275cf45
jsta <stachel2@msu.edu>
2019-09-20 12:24:44 -0400

    towards collating nla data

--- scripts/00_get_nla.R
+++ scripts/00_get_nla.R
@@ -14,6 +14,7 @@ nl2012 <- nla_load(2012)$wide_siteinfo %>%
   distinct(SITE_ID, .keep_all = TRUE) %>%
   left_join(dplyr::select(nla_load(2012)$wide_profile,
                           SITE_ID, INDEX_SITE_DEPTH)) %>%
+  # TODO: pull lake names?
   tidyr::drop_na(INDEX_SITE_DEPTH) %>%
   group_by(SITE_ID) %>%
   slice(which.max(INDEX_SITE_DEPTH)) %>%
@@ -23,11 +24,17 @@ dt_2012 <- dt_raw_2012 %>%
   distinct(SITE_ID, .keep_all = TRUE) %>%
   left_join(nl2012) %>%
   dplyr::filter(!is.na(INDEX_SITE_DEPTH)) %>%
-  data.frame(stringsAsFactors = FALSE)
+  data.frame(stringsAsFactors = FALSE) %>%
+  janitor::clean_names("snake") %>%
+  rename(llid = linked_lagoslakeid)
+
 
 dt_2007 <- dt_raw_2007 %>%
   distinct(SITE_ID, .keep_all = TRUE) %>%
   dplyr::filter(!is.na(DEPTHMAX)) %>%
   data.frame(stringsAsFactors = FALSE)
 
+# deal with 2007/2012 dups?
 
+# anticipated columns:
+# llid, name, legacy_name, state, max_depth_m, mean_depth_m, source, lat, long
