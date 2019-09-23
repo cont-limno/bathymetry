bdaec5dd8e4160c9e1b0ae7bc1016e3ebec5cc12
jsta <stachel2@msu.edu>
2019-09-23 11:31:38 -0400

    add lagosne to pipeline

--- Makefile
+++ Makefile
@@ -2,10 +2,14 @@
 all: data
 
 data: data/00_manual/00_manual.csv \
-data/00_nla/00_nla.csv
+data/00_nla/00_nla.csv \
+data/00_lagosne/00_lagosne.csv
 
 data/00_manual/00_manual.csv: scripts/00_get_manual.R
 	Rscript $<
 
 data/00_nla/00_nla.csv: scripts/00_get_nla.R
+	Rscript $<
+
+data/00_lagosne/00_lagosne.csv: scripts/00_get_lagosne.R
 	Rscript $<
--- scripts/00_get_lagosne.R
+++ scripts/00_get_lagosne.R
@@ -1,3 +1,18 @@
+source("scripts/99_utils.R")
 
-# cross-walk lagosne llids with lagosus llids
-# select depth data
+lg <- lagosne_load("1.087.3")
+
+# anticipated columns:
+# llid, name, legacy_name, state, max_depth_m, mean_depth_m, source, lat, long
+res <- mutate(lg$lakes_limno,
+       legacy_name = NA) %>%
+  left_join(dplyr::select(lg$locus, lagoslakeid, gnis_name, state_zoneid)) %>%
+  left_join(dplyr::select(lg$state, state, state_zoneid)) %>%
+dplyr::select(lagoslakeid, name = gnis_name, state,
+              max_depth_m = maxdepth, mean_depth_m = meandepth,
+              source = maxdepthsource, lat = nhd_lat, long = nhd_long) %>%
+  # TODO: eventually cross-walk lagosne llids with lagosus llids
+  dplyr::mutate(lagoslakeid = NA) %>%
+  dplyr::filter(!is.na(max_depth_m) | !is.na(mean_depth_m))
+
+write.csv(res, "data/00_lagosne/00_lagosne.csv", row.names = FALSE)
