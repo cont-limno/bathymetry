71fda927faa856545b32023d68a8dcf46cd20bd2
jsta <stachel2@msu.edu>
2019-10-14 12:04:22 -0400

    typo fix

--- scripts/00_get_hypso_mi.R
+++ scripts/00_get_hypso_mi.R
@@ -8,8 +8,8 @@ download.file("https://opendata.arcgis.com/datasets/d49160d2e5af4123b15d48c2e9c7
 
 lg_poly <- query_gis_(query = paste0("SELECT * FROM LAGOS_NE_All_Lakes_4ha WHERE ",
                                    paste0("State_Name LIKE '", "Michigan'", collapse = " OR ")))
-ct      <- st_read("data/mi_bathy/contours.geojson")
-ct      <- st_transform(ct, st_crs(lg_poly))
+mi      <- st_read("data/mi_bathy/contours.geojson")
+mi      <- st_transform(mi, st_crs(lg_poly))
 
 ct_large <- st_join(ct, lg_poly) %>%
   dplyr::filter(!is.na(lagoslakeid)) %>%
