fdc681ad01fbfd292bd2b31adb6a35785f96e78b
jsta <stachel2@msu.edu>
2019-11-11 14:53:00 -0500

    towards pulling mi lake data

--- scripts/00_get_hypso_mi.R
+++ scripts/00_get_hypso_mi.R
@@ -10,14 +10,33 @@ lg_poly <- query_gis_(query = paste0("SELECT * FROM LAGOS_NE_All_Lakes_4ha WHERE
                                    paste0("State_Name LIKE '", "Michigan'", collapse = " OR ")))
 mi      <- st_read("data/mi_bathy/contours.geojson")
 mi      <- st_transform(mi, st_crs(lg_poly))
+mi      <- st_join(mi, lg_poly) %>%
+  dplyr::filter(!is.na(lagoslakeid))
 
-ct_large <- st_join(ct, lg_poly) %>%
-  dplyr::filter(!is.na(lagoslakeid)) %>%
+# mi %>%
+# group_by(lagoslakeid) %>%
+# tally()
+
+mi_large <- mi %>%
   mutate(area = st_area(.)) %>%
   group_by(lagoslakeid) %>%
   slice(which.max(area)) %>%
   ungroup()
 
+mi_large_filled       <- lapply(seq_len(nrow(mi_large)), function(x)
+  concaveman::concaveman(st_cast(mi_large[x,], "POINT"))
+)
+mi_large_filled       <- do.call(rbind, mi_large_filled)
 
+st_geometry(mi_large) <- mi_large_filled$polygons
+mi_large              <- mutate(mi_large,
+                                legacy_id = 1:nrow(mi_large))
+mi_large <- mi_large[st_is_valid(mi_large),]
 
-
+# for each mi_large find intersecting mi
+# assign corresponding llid
+test <- lapply(seq_len(nrow(mi_large)[1:50]), function(i){
+  # i <- 1
+  jsta::get_intersects(mi, mi_large[i,])
+})
+lapply(test, nrow)
