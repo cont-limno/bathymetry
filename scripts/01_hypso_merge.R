01edd7eb7060b4f355dcd819d05a76828a87f76d
jsta <stachel2@msu.edu>
2019-12-16 10:37:41 -0500

    add state column

--- scripts/01_hypso_merge.R
+++ scripts/01_hypso_merge.R
@@ -1,8 +1,11 @@
 source("scripts/99_utils.R")
 
-hypso_ct <- read.csv("data/ct_hypso.csv", stringsAsFactors = FALSE)
-hypso_mn <- read.csv("data/mn_hypso.csv", stringsAsFactors = FALSE)
-hypso_mi <- read.csv("data/mi_hypso.csv", stringsAsFactors = FALSE)
+hypso_ct <- read.csv("data/ct_hypso.csv", stringsAsFactors = FALSE) %>%
+  mutate(state = "CT")
+hypso_mn <- read.csv("data/mn_hypso.csv", stringsAsFactors = FALSE) %>%
+  mutate(state = "MN")
+hypso_mi <- read.csv("data/mi_hypso.csv", stringsAsFactors = FALSE) %>%
+  mutate(state = "MI")
 
 # hypso_ks <- read.csv("data/ks_hypso.csv", stringsAsFactors = FALSE)
 # hypso_nh <- read.csv("data/nh_hypso.csv", stringsAsFactors = FALSE)
