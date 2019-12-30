5b7d355c0d5973004fc6d59ea23a04ddf0239c6b
jsta <stachel2@msu.edu>
2019-12-30 10:36:15 -0500

    compare manual versus lagosne slopes

--- scripts/00_get_nearshore.R
+++ scripts/00_get_nearshore.R
@@ -1,17 +1,15 @@
 source("scripts/99_utils.R")
-
-dt              <- read.csv("data/lagosus_depth.csv",
-                            stringsAsFactors = FALSE)
-
-# lagos geo module should have terrain metrics for mea slope
+# lagos geo module should have terrain metrics for mean slope
 # Oliver (year) found that max slope was most informative
 
+dt      <- read.csv("data/lagosus_depth.csv",
+                    stringsAsFactors = FALSE)
+dt_pred <- read.csv("data/lagosus_depth_predictors.csv",
+                    stringsAsFactors = FALSE)
 
-# attempt at manual calculation follows:
+# attempt at manual calculation of max buffer slope
 max_buffer_dist <- 100 # to match Hollister (2011)
-
-llid <- 4040 # test on one lake for now
-# lake_info(llid)
+llid            <- 4040
 
 # pull lake outline with LAGOSNEgis
 ll_poly <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", llid)
@@ -19,10 +17,13 @@ ll_poly <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", llid)
 ll_iws <- query_gis("IWS", "lagoslakeid", llid)
 
 # pull buffer with elevatr
-elev      <- get_elev_raster(as_Spatial(ll_iws),
-                        13, clip = "bbox")
-
-# TODO: cache get_elev_raster output because FedData results have terribly low res
+if(!file.exists("elev.tif")){
+  elev      <- get_elev_raster(as_Spatial(ll_iws),
+                          13, clip = "bbox")
+  writeRaster(elev, "elev.tif")
+}else{
+  elev <- raster("elev.tif")
+}
 
 # approximates functions in lakemorpho package
 elev      <- mask(elev, as_Spatial(ll_iws))
@@ -30,16 +31,8 @@ elev      <- mask(elev, as_Spatial(st_buffer(ll_poly, max_buffer_dist)))
 slope     <- terrain(elev, "slope")@data@values
 slope_med <- median(slope, na.rm = TRUE)
 
+max(slope, na.rm = TRUE) * res(elev)[2]
+dplyr::filter(dt_pred, lagoslakeid == llid) %>%
+  pull(buffer100m_slope_max) / 10 # lagosne slopes are per 10m
 
-library(lakemorpho)
-test <- lakeSurroundTopo(as_Spatial(ll_poly), elev)
-
-
-#   test <- mask(test, test <= (cellStats(test, "min") + 1), maskvalue = TRUE)
-#   test <- test - cellStats(test, "min")
-#   test <- resample(test, rsub)
-#
-#   test2 <- merge(test, rsub)
-#   test2[is.na(test2)] <- 0
-
-
+# 3.63 vs 3.29 lagosne
