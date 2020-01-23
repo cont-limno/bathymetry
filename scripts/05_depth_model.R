3dde34c0552eb72fe6f6658518a0f11d381aa6e9
jsta <stachel2@msu.edu>
2020-01-23 13:18:52 -0500

    fit oliver 2016 model

--- scripts/05_depth_model.R
+++ scripts/05_depth_model.R
@@ -76,9 +76,56 @@ ggplot() +
 saveRDS(res, "data/01_depth_model/depth_model.rds")
 
 # ---- Oliver et al. 2016 random effects model ----
+library(dplyr)
+# setwd("../")
+oliver2015 <- LAGOSNE::lagos_load_oliver_2015() %>%
+  mutate(lake_maxdepth_m_oliver = zmaxpredict)
+dt         <- read.csv("data/lagosne_depth_predictors.csv",
+               stringsAsFactors = FALSE) # %>%
+  # dplyr::filter(shape_class %in% c("convex", "concave"))
 
-dt <- read.csv("data/lagosne_depth_predictors.csv", stringsAsFactors = FALSE)
+## select specific predictors
+dt <- dplyr::select(dt, lake_maxdepth_m,
+                    buffer100m_slope_max, lake_waterarea_ha,
+                    lake_shorelinedevfactor_nounits,
+                    ws_lake_arearatio,
+                    shape_class, hu4_zoneid, lagoslakeid) %>%
+  mutate(lagoslakeid = as.character(lagoslakeid))
 
+## standardize predictors and response vars
+# all predictors were log transformed, centered, and scaled
+# depth was log transformed
+scale2 <- function(x, na.rm = TRUE){
+  (x - mean(x, na.rm = na.rm)) / sd(x, na.rm)
+}
+dt <- dt %>%
+  dplyr::mutate_if(is.numeric, log) %>%
+  dplyr::mutate_at(vars(buffer100m_slope_max:ws_lake_arearatio),
+                   scale2)
 
+## create a holdout dataset using 10% of the data from each hu4
+train <- group_by(dt, hu4_zoneid) %>% dplyr::sample_frac(.9)
+test  <- dplyr::anti_join(dt, train, by = 'lagoslakeid')
 
+## fit model
+library(lme4)
+eq <- as.formula("lake_maxdepth_m ~ buffer100m_slope_max + lake_waterarea_ha + ws_lake_arearatio + lake_shorelinedevfactor_nounits + (lake_waterarea_ha | hu4_zoneid)")
+fit <- lme4::lmer(eq, data = train, REML = FALSE)
 
+## predict on holdouts
+test$lake_maxdepth_m_predicted <- predict(fit, test)
+plot(test$lake_maxdepth_m_predicted, test$lake_maxdepth_m, xlim = c(0, 5), ylim = c(0, 5))
+abline(0, 1)
+
+## calculate rmse
+yardstick::rmse(test, exp(lake_maxdepth_m), exp(lake_maxdepth_m_predicted))
+yardstick::rsq(test, exp(lake_maxdepth_m), exp(lake_maxdepth_m_predicted))
+
+test <- test %>%
+  left_join(dplyr::select(dplyr::mutate(oliver2015, lagoslakeid = as.character(lagoslakeid)), lagoslakeid,
+                          lake_maxdepth_m_oliver))
+plot(exp(test$lake_maxdepth_m_predicted), test$lake_maxdepth_m_oliver)
+abline(0, 1)
+
+plot(oliver2015$zmaxpredict, oliver2015$zmaxobs)
+abline(0, 1)
