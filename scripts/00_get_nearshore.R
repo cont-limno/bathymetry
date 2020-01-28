7929a408768840314decd4260a1e4cb123fe9955
jsta <stachel2@msu.edu>
2020-01-28 17:38:34 -0500

    pull some test nearshore online slope values

--- scripts/00_get_nearshore.R
+++ scripts/00_get_nearshore.R
@@ -7,16 +7,21 @@ dt         <- read.csv("data/lagosus_depth.csv",
 bathy_pnts <- readRDS("data/00_bathy_depth/bathy_pnts.rds")
 dt_pred    <- read.csv("data/lagosne_depth_predictors.csv",
                     stringsAsFactors = FALSE) %>%
-  dplyr::filter(lagoslakeid %in% bathy_pnts$llid)
-
+  dplyr::filter(lagoslakeid %in% bathy_pnts$llid &
+                  !is.na(reservoir_class))
 
 # attempt at manual calculation of max buffer slope
 max_buffer_dist <- 100 # to match Hollister (2011)
 set.seed(55)
-ll_ids <- sample(dt_pred$lagoslakeid, 10)
+ll_ids <- sample(dt_pred$lagoslakeid, 20)
+flist <- list.files("data/elevatr", pattern = "\\d*.tif",
+                    full.names = TRUE, include.dirs = TRUE)
+ll_ids <- gsub(".tif", "",
+                stringr::str_extract(flist, "\\d*(!?.tif)"))
+# sapply(flist[!(ll_ids %in% dt_pred$lagoslakeid)], "unlink")
 
 get_slope <- function(ll_id){
-  # ll_id <- ll_ids[2]
+  # ll_id <- ll_ids[1]
   print(ll_id)
   ll_poly <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", ll_id)
   ll_iws <- query_gis("IWS", "lagoslakeid", ll_id)
@@ -35,8 +40,8 @@ get_slope <- function(ll_id){
   elev      <- mask(elev, as_Spatial(st_buffer(ll_poly, max_buffer_dist)))
   slope     <- terrain(elev, "slope")
 
-  slope_mean <- mean(slope@data@values, na.rm = TRUE)
-  slope_max <- max(slope@data@values, na.rm = TRUE) # * res(elev)[2]
+  slope_mean <- mean(slope@data@values, na.rm = TRUE) * res(elev)[1]
+  slope_max <- max(slope@data@values, na.rm = TRUE) * res(elev)[1]
   slope_ne  <- dplyr::filter(dt_pred, lagoslakeid == ll_id) %>%
     dplyr::select(buffer100m_slope_mean, buffer100m_slope_max) %>%
     mutate_all(function(x) x / 10) # lagosne slopes are per 10m
@@ -56,15 +61,26 @@ get_slope <- function(ll_id){
   buffer_line <- sf::st_nearest_points(shore_pnt,
                                  st_transform(st_cast(ll_buf, "MULTILINESTRING"), st_crs(r)))
   slope_online <- raster::extract(slope, as_Spatial(buffer_line))
-  slope_online <- mean(slope_online[[1]], na.rm = TRUE)#  * res(slope)[2]
+  slope_online <- mean(slope_online[[1]], na.rm = TRUE) * res(slope)[2]
 
   res <- list(slope_mean = slope_mean, slope_max = slope_max,
        slope_max_ne = slope_ne$buffer100m_slope_max,
        slope_mean_ne = slope_ne$buffer100m_slope_mean,
        slope_online = slope_online)
   res <- bind_rows(res) %>%
-    mutate(llid = ll_id)
+    mutate(llid = as.character(ll_id))
   res
 }
 
-test <- lapply(ll_ids, get_slope)
+res <- lapply(ll_ids, get_slope) %>%
+  bind_rows() %>%
+  left_join(dplyr::select(sf::st_drop_geometry(bathy_pnts),
+                          llid, inlake_slope))
+
+# write to csv
+write.csv(res, "data/00_geometry/nearshore.csv", row.names = FALSE)
+
+cor.test(test2$slope_mean, test2$inlake_slope)
+cor.test(test2$slope_online, test2$inlake_slope)
+
+hist(test2$inlake_slope / test2$slope_online)
