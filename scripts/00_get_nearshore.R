6bd6fd7f48cf37a3e4e5af64b1669a46a5e3ae50
jsta <stachel2@msu.edu>
2020-02-14 10:23:43 -0500

    correctly pull buffer slopes

--- scripts/00_get_nearshore.R
+++ scripts/00_get_nearshore.R
@@ -12,12 +12,12 @@ bathy_pnts <- readRDS("data/00_bathy_depth/bathy_pnts.rds")
 dt_pred    <- read.csv("data/lagosne_depth_predictors.csv",
                     stringsAsFactors = FALSE) %>%
   dplyr::filter(lagoslakeid %in% bathy_pnts$llid &
-                  !is.na(reservoir_class))
+                  !is.na(shape_class))
 
 # attempt at manual calculation of max buffer slope
 
-ll_ids <- sample(dt_pred$lagoslakeid, 40)
-flist <- list.files("data/elevatr", pattern = "\\d*.tif",
+ll_ids            <- sample(dt_pred$lagoslakeid, 60)
+flist             <- list.files("data/elevatr", pattern = "\\d*.tif",
                     full.names = TRUE, include.dirs = TRUE)
 existing_surfaces <- gsub(".tif", "",
                 stringr::str_extract(flist, "\\d*(!?.tif)"))
@@ -28,7 +28,10 @@ ll_ids <- ll_ids[!duplicated(ll_ids)]
 get_slope <- function(ll_id){
   # ll_id <- ll_ids[1]
   ll_poly <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", ll_id)
-  ll_iws <- query_gis("IWS", "lagoslakeid", ll_id)
+  ll_iws  <- query_gis("IWS", "lagoslakeid", ll_id)
+  # TODO: make this a one-sided buffer [sf doesn't offer this :(]
+  ll_buff <- st_buffer(ll_poly, max_buffer_dist)
+  ll_buff <- st_difference(ll_buff, ll_poly)
 
   fname <- paste0("data/elevatr/", ll_id, ".tif")
   if(!file.exists(fname)){
@@ -42,7 +45,7 @@ get_slope <- function(ll_id){
 
   # approximates functions in lakemorpho package
   elev      <- mask(elev, as_Spatial(ll_iws))
-  elev      <- mask(elev, as_Spatial(st_buffer(ll_poly, max_buffer_dist)))
+  elev      <- mask(elev, as_Spatial(ll_buff))
   slope     <- terrain(elev, "slope")
 
   slope_mean   <- mean(slope@data@values, na.rm = TRUE) * res(elev)[1]
@@ -53,30 +56,37 @@ get_slope <- function(ll_id){
     mutate_all(function(x) x / 10) # lagosne slopes are per 10m
   slope_ne     <- slope_ne[1,]
 
-  # pull slope on-line from deepest point
+  # pull buffer slope on-line from deepest point
   deepest_pnt         <- dplyr::filter(bathy_pnts, llid == ll_id)
-  r <- raster(
+  r_crs               <- raster( # only used to set crs
     paste0("data/", tolower(deepest_pnt$state), "_bathy/", ll_id, ".tif")
     )
-  st_crs(deepest_pnt) <- st_crs(r)
+  st_crs(deepest_pnt) <- st_crs(r_crs)
 
-  shore_pnt <- sf::st_nearest_points(deepest_pnt, st_transform(
-    st_cast(ll_poly, "MULTILINESTRING"), st_crs(r)))
-  ll_buf    <- st_buffer(ll_poly, 100)
-
+  ll_poly_hull <- st_zm(st_cast(
+    concaveman::concaveman(st_cast(ll_poly, "MULTILINESTRING")),
+    "MULTILINESTRING"))
+  ll_buff_hull <- st_zm(st_cast(
+    concaveman::concaveman(st_cast(ll_buff, "MULTILINESTRING")),
+    "MULTILINESTRING"))
+  shore_pnt <- sf::st_nearest_points(deepest_pnt,
+                                     st_transform(ll_poly_hull, st_crs(r_crs)))
   buffer_line  <- sf::st_nearest_points(shore_pnt,
-                                 st_transform(st_cast(ll_buf, "MULTILINESTRING"), st_crs(r)))
-  slope_online <- mask(slope, as_Spatial(buffer_line))
-  slope_online <- raster::extract(slope, as_Spatial(buffer_line))
-  slope_online_mean <- mean(slope_online[[1]], na.rm = TRUE) * res(slope)[2]
-  slope_online_median <- median(slope_online[[1]], na.rm = TRUE) * res(slope)[2]
+                                        st_transform(ll_buff_hull, st_crs(r_crs)))
 
-  res <- list(slope_mean = slope_mean, slope_max = slope_max,
-              slope_median = slope_median,
-       slope_max_ne = slope_ne$buffer100m_slope_max,
-       slope_mean_ne = slope_ne$buffer100m_slope_mean,
-       slope_online_mean = slope_online_mean,
-       slope_online_median = slope_online_median)
+  # mapview(elev) + mapview(ll_poly_hull) + mapview(ll_buff_hull) + mapview(buffer_line)
+
+  slope_online        <- raster::extract(slope, as_Spatial(buffer_line))[[1]]
+  slope_online_mean   <- mean(slope_online, na.rm = TRUE) * res(elev)[1]
+  slope_online_median <- median(slope_online, na.rm = TRUE) * res(elev)[1]
+
+  res <- list(slope_mean = slope_mean, slope_max = slope_max, # buffer
+       slope_median = slope_median, # buffer
+       slope_max_ne = slope_ne$buffer100m_slope_max, # buffer
+       slope_mean_ne = slope_ne$buffer100m_slope_mean, # buffer
+       slope_online_mean = slope_online_mean, # buffer
+       slope_online_median = slope_online_median,
+       res_elev_1 = res(elev)[1]) # buffer
   res <- bind_rows(res) %>%
     mutate(llid = as.character(ll_id))
   res
