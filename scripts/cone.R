2a91d9b50272a87f630b7677564869202344073f
jsta <stachel2@msu.edu>
2020-02-19 16:56:26 -0500

    stash to prep for repo split

--- scripts/cone.R
+++ scripts/cone.R
@@ -4,4 +4,10 @@ library(sf)
 
 test <- lapply(1:40, function(x) st_buffer(p1, x))
 test <- st_multipolygon(test)
-plot(test)
+test2 <- st_sfc(test)
+test2 <- st_cast(test, "POLYGON")
+plot(test2)
+
+r <- raster::rasterize(as_Spatial(st_sfc(test)))
+
+# https://stackoverflow.com/questions/11945604/3d-cone-and-cylinder-in-r
