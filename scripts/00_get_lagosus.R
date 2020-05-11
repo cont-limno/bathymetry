93d94b580101eccc182bc2d8a67cdb9fe1a29687
jsta <stachel2@msu.edu>
2020-05-11 17:06:32 -0400

    use all depths lagosus product

--- scripts/00_get_lagosus.R
+++ scripts/00_get_lagosus.R
@@ -1,6 +1,9 @@
 source("scripts/99_utils.R")
 
-lg <- LAGOSUS::lagosus_load("depth")
-lg <- lg$depth$depth
+# lg <- LAGOSUS::lagosus_load("depth")
+# lg <- lg$depth$depth
+
+# use all depths product
+lg <- read.csv("../lagos_depth/data/lagosus_depth_all.csv", stringsAsFactors = FALSE)
 
 write.csv(lg, "data/lagosus_depth.csv", row.names = FALSE)
