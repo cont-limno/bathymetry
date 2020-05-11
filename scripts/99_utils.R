066ef088be9de3f77e35002302b84aa76b81e614
jsta <stachel2@msu.edu>
2020-05-11 17:04:11 -0400

    list more package dependencies, remove jsta package dependence

--- scripts/99_utils.R
+++ scripts/99_utils.R
@@ -15,17 +15,25 @@ suppressMessages(library(cowplot))
 library(cutr) # devtools::install_github("moodymudskipper/cutr")
 library(broom)
 suppressMessages(library(raster))
-library(LAGOSNEgis)
+library(LAGOSNEgis) # install_github("cont-limno/LAGOSNEgis")
 suppressMessages(library(scales))
 library(elevatr)
 suppressMessages(library(dplyr))
 library(progress)
-suppressMessages(library(LAGOSUS))
+suppressMessages(library(LAGOSUS)) # install_github("cont-limno/LAGOSUS")
 library(polylabelr)
 library(esri2sf) # install_github("yonghah/esri2sf")
 library(rnaturalearth)
 library(mapview)
-library(ggrastr)
+library(ggrastr) # install_github('VPetukhov/ggrastr')
+library(lwgeom)
+library(rmapshaper)
+library(kableExtra)
+library(LAGOSUSgis) # install_github("cont-limno/LAGOSUSgis")
+library(rgdal)
+library(rnaturalearthhires) # install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+library(GGally)
+library(ranger)
 
 # ---- misc fxn ----
 # jsta::get_if_not_exists
@@ -279,3 +287,49 @@ is_url <- function(x){
 # area_ratio <- st_area(
 #   lwgeom::st_minimum_bounding_circle(concaveman::concaveman(dt))) /
 #   st_area(concaveman::concaveman(dt))
+
+# jsta::jheatmap
+jheatmap <- function(dt, focal_columns = NULL) {
+  test <- data.frame(dt) %>% dplyr::select_if(is.numeric) %>%
+    corrr::correlate(quiet = TRUE) %>% data.frame()
+  test_rowname <- test$rowname
+  test <- dplyr::select(test, -"rowname")
+  row.names(test) <- test_rowname
+  if (length(focal_columns) > 0) {
+    test <- dplyr::select(test, focal_columns)
+  }
+  hmap.palette_red <- colorRampPalette(RColorBrewer::brewer.pal(n = 7,
+                                                                name = "Reds"))
+  hmap.palette_blue <- colorRampPalette(RColorBrewer::brewer.pal(n = 7,
+                                                                 name = "Blues"))
+  frac_color <- ceiling(range(as.numeric(unlist(test)), na.rm = TRUE) *
+                          10)
+  frac_pos <- frac_color[which(frac_color > 0)]
+  frac_neg <- frac_color[which(frac_color < 0)]
+  if (length(frac_neg) == 0) {
+    hmap_cols <- rev(c(rev(hmap.palette_red(frac_pos))))
+  }
+  else {
+    if (length(frac_pos) == 0) {
+      hmap_cols <- rev(c(hmap.palette_blue(abs(frac_neg))))
+    }
+    else {
+      hmap_cols <- rev(c(rev(hmap.palette_red(frac_pos)),
+                         hmap.palette_blue(abs(frac_neg))))
+    }
+  }
+  if (length(focal_columns) == 1) {
+    test_rowname <- row.names(test)[order(test[, focal_columns])]
+    test <- arrange(test, rlang::UQ(rlang::sym(focal_columns))) %>%
+      dplyr::filter(!is.na(rlang::UQ(rlang::sym(focal_columns))))
+    test_rowname <- test_rowname[test_rowname != focal_columns]
+    row.names(test) <- test_rowname
+  }
+  if (length(focal_columns) > 0) {
+    suppressWarnings(pheatmap::pheatmap(test, color = hmap_cols,
+                                        cluster_rows = FALSE, cluster_cols = FALSE))
+  }
+  else {
+    pheatmap::pheatmap(t(test), color = hmap_cols)
+  }
+}
