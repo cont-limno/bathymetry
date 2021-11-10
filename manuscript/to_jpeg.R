f13e32441b0500b2cf6e6e1c2aa373840c548b95
jsta <stachel2@msu.edu>
2021-11-10 13:59:43 -0700

    jpeg conversion workflow

---
+++ manuscript/to_jpeg.R
@@ -0,0 +1,13 @@
+library(pdftools)
+
+fig_list <- paste0("Figure_", 1:5)
+in_paths <- paste0("inland_waters/", fig_list, ".pdf")
+out_paths <- paste0("inland_waters/", fig_list, ".jpeg")
+
+make_jpeg <- function(in_path, out_path) {
+  bitmap <- pdf_render_page(in_path, page = 1, dpi = 600)
+  jpeg::writeJPEG(bitmap, out_path)
+}
+
+lapply(seq_len(length(fig_list)),
+  function(i) make_jpeg(in_paths[i], out_paths[i]))
